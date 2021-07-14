048c4a7c50721ade7d2e413eadb540a73090c9be
Dario <dario.defrancisci92@gmail.com>
2021-07-14 13:57:36 +0200

    Caricato: variabili

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Chiedi all'utente il suo: nome - cognome - colore preferito
+
+var nome = prompt("Inserisci il tuo nome");
+
+var cognome = prompt("Inserisci il tuo cognome");
+
+var colore = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("nome").innerHTML = nome + cognome + colore + 21;
